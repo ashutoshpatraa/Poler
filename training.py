758e38b30c32a2f998984303dfecfb0bcf9ab612
Ashutosh Patra <ashutoshpatra616@gmail.com>
2024-09-20 22:10:06 +0530

    /

--- training.py
+++ training.py
@@ -8,7 +8,8 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
 import random
-words=[]
+
+words = []
 classes = []
 documents = []
 ignore_words = ['?', '!']
@@ -17,27 +18,29 @@ intents = json.loads(data_file)
 
 for intent in intents['intents']:
     for pattern in intent['patterns']:
-        #tokenize each word
+        # tokenize each word
         w = nltk.word_tokenize(pattern)
         words.extend(w)
-        #add documents in the corpus
+        # add documents in the corpus
         documents.append((w, intent['tag']))
         # add to our classes list
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
+
 # lemmatize and lower each word and remove duplicates
 words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
 words = sorted(list(set(words)))
 # sort classes
 classes = sorted(list(set(classes)))
 # documents = combination between patterns and intents
-print (len(documents), "documents")
+print(len(documents), "documents")
 # classes = intents
-print (len(classes), "classes", classes)
+print(len(classes), "classes", classes)
 # words = all words, vocabulary
-print (len(words), "unique lemmatized words", words)
-pickle.dump(words,open('texts.pkl','wb'))
-pickle.dump(classes,open('labels.pkl','wb'))
+print(len(words), "unique lemmatized words", words)
+pickle.dump(words, open('texts.pkl', 'wb'))
+pickle.dump(classes, open('labels.pkl', 'wb'))
+
 # create our training data
 training = []
 # create an empty array for our output
@@ -53,19 +56,22 @@ for doc in documents:
     # create our bag of words array with 1, if word match found in current pattern
     for w in words:
         bag.append(1) if w in pattern_words else bag.append(0)
-    
+
     # output is a '0' for each tag and '1' for current tag (for each pattern)
     output_row = list(output_empty)
     output_row[classes.index(doc[1])] = 1
-    
+
     training.append([bag, output_row])
+
 # shuffle our features and turn into np.array
 random.shuffle(training)
-training = np.array(training)
+training = np.array(training, dtype=object)
+
 # create train and test lists. X - patterns, Y - intents
-train_x = list(training[:,0])
-train_y = list(training[:,1])
+train_x = np.array([item[0] for item in training])
+train_y = np.array([item[1] for item in training])
 print("Training data created")
+
 # Create model - 3 layers. First layer 128 neurons, second layer 64 neurons and 3rd output layer contains number of neurons
 # equal to number of intents to predict output intent with softmax
 model = Sequential()
@@ -74,10 +80,12 @@ model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
+
 # Compile model. Stochastic gradient descent with Nesterov accelerated gradient gives good results for this model
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-#fitting and saving the model 
+
+# fitting and saving the model
 hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
 model.save('model.h5', hist)
 print("model created")
